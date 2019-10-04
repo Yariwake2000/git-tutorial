5e3305dfc921a3dcc9d45b8f1ef6eb9754cbc3a3
yariwake <yariwake@j1639.epi.it.matsue-ct.jp>
2019-10-04 16:27:23 +0900

    FIZ for feature-fz

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+#! /usr/bin/env ruby
+
+(1..100).each{|i|
+		if (i % 3 == 0 && i % 5 == 0){
+			printf("FizzBuzz\n");
+		} 
+		else if (i % 3 == 0) {
+			printf("Fizz\n");
+		} 
+		else if (i % 5 == 0) {
+			printf("Buzz\n");
+		} 
+		else {
+			printf("%d\n", i);
+		}
+}
