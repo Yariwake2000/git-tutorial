7cc6cfc73c7b7b56ece834403333b563516402fd
yariwake <yariwake@j1639.epi.it.matsue-ct.jp>
2019-10-04 18:37:17 +0900

    FIX 7 feature-fz7

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+#! /usr/bin/env ruby
+
+(1..100).each{|i|
+
+	if i % 3 == 0 && i % 5 == 0 then
+		print "FizzBuzz\n"
+	elsif i % 3 == 0 then
+		print "Fizz\n"
+	elsif i % 5 == 0 then
+		print "Buzz\n"
+	elsif i % 7 == 0 then
+		print "git\n"
+	end
+
+}
